418748401b7ad9f9f80938126fe718ccf237b912
Aldenor Junior <aldenor.junior@tce.ro.gov.br>
2016-07-15 17:29:32 -0400

    Add view login and logic on controlle facebook auth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,24 @@
 angular.module('app.controllers', [])
-  
+
+.controller('loginCtrl', function($scope) {
+  $scope.fbLogin = function() {
+    openFB.login(
+      function(response) {
+        if (response.status === 'connected') {
+          console.log('Facebook login succeeded');
+          $scope.closeLogin();
+        } else {
+          alert('Facebook login failed');
+        }
+      },
+      {scope: 'email,publish_actions'});
+  }
+})
+
 .controller('pOCtrl', function($scope) {
 
 })
-   
+
 .controller('leiteCtrl', function($scope) {
 
 })
-    
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,43 +1,55 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+// .config(
+//   //openFB.init({appId: '123456'})
+// )
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+.config(
+  function($stateProvider, $urlRouterProvider) {
+    openFB.init({appId: '1722281661359340'});
 
-      .state('tabsController.pO', {
-    url: '/pao',
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+  .state('main', {
+    url: '/page',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('main.login', {
+    url: '/login',
     views: {
       'tab1': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('main.pO', {
+    url: '/pao',
+    views: {
+      'tab2': {
         templateUrl: 'templates/pO.html',
         controller: 'pOCtrl'
       }
     }
   })
 
-  .state('tabsController.leite', {
+  .state('main.leite', {
     url: '/leite',
     views: {
-      'tab2': {
+      'tab3': {
         templateUrl: 'templates/leite.html',
         controller: 'leiteCtrl'
       }
     }
   })
 
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
-  })
-
-$urlRouterProvider.otherwise('/page1/pao')
-
-  
+  $urlRouterProvider.otherwise('/page/login')
 
 });
