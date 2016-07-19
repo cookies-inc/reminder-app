d613698f17ac1cf4778c21ddd0688e033a4101e8
Aldenor Junior <aldenor.junior@tce.ro.gov.br>
2016-07-19 08:35:30 -0400

    Set getUserOfWeek resource; And create new setDone method

--- www/js/services.js
+++ www/js/services.js
@@ -4,10 +4,18 @@ angular.module('reminder.services', ['ngResource'])
 	.constant("baseURL","http://localhost:3000/")
 	.service('userService', ['$resource', 'baseURL',
 		function($resource,baseURL){
+			
 			this.getUsersOfWeek = function() {
 
-			  return $resource(baseURL+"users");
+			  return $resource(baseURL+"week/users");
 
+			};
+
+			this.setDone = function(data) {
+				//console.log(data);
+				return $resource(baseURL+"historical",{}, {
+					update : { method: "POST", params: data }}
+				);
 			};
 		}
 	])
